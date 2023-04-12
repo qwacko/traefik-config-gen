759ebdf3b8e4e715e873140284bc9b230f59bea0
qwacko <james@jmcdowall.com>
2023-04-12 19:20:17 +0000

    Added initial output functionality

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,11 @@
+import { prisma } from '$lib/server/db';
 import { auth } from '$lib/server/lucia';
 import { createContext } from '$lib/server/trpc/context';
 import { router } from '$lib/server/trpc/router';
 import type { Handle } from '@sveltejs/kit';
 import { sequence } from '@sveltejs/kit/hooks';
 import { createTRPCHandle } from 'trpc-sveltekit';
+import url from 'url';
 
 const authHandler: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
@@ -28,7 +30,30 @@ const trpcInEvent: Handle = async ({ event, resolve }) => {
 	return await resolve(event);
 };
 
+const checkOutput: Handle = async ({ event, resolve }) => {
+	// Only execute if the address is not a route. AVoids overhead when not necessary
+	if (!event.route.id) {
+		const outputAddressses = await prisma.outputConfig.findMany({
+			select: { id: true, address: true }
+		});
+		const targetAddress = url.parse(event.request.url).pathname;
+		const outputConfig = outputAddressses.find((o) => `/${o.address}` === targetAddress);
+
+		if (outputConfig) {
+			const outputText = await prisma.outputHistory.findFirst({
+				where: { configId: outputConfig.id, current: true },
+				orderBy: { createdAt: 'desc' }
+			});
+			return new Response(outputText?.output || '');
+		}
+	}
+	console.timeEnd();
+
+	return await resolve(event);
+};
+
 export const handle = sequence(
+	checkOutput,
 	authHandler,
 	createTRPCHandle({ router, createContext }),
 	trpcInEvent
