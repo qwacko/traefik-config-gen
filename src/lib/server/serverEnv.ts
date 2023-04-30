aaabb88294f14f1da87930d2beec31da3e4ec831
qwacko <james@jmcdowall.com>
2023-04-30 04:00:33 +0000

    Improved Debugging

--- src/lib/server/serverEnv.ts
+++ src/lib/server/serverEnv.ts
@@ -9,7 +9,13 @@ const serverEnvValidation = z
 		DEV: z.coerce.boolean(),
 		CSRF_CHECK_ORIGIN: z.coerce.boolean(),
 		CONFIG_FILE: z.string().optional().default('./config.yaml'),
-		DEBUG: z.coerce.boolean().optional()
+		DEBUG: z.coerce.boolean().optional(),
+		// DEBUG_CLASSES is a string containing an array of logging levels to enable ("ERROR","WARN","INFO","DEBUG","TRACE")
+		DEBUG_CLASSES: z
+			.string()
+			.optional()
+			.default('ERROR')
+			.transform((data) => data.split(',').map((d) => d.trim().toUpperCase()))
 	})
 	.transform((data) => ({
 		...data,
@@ -23,7 +29,32 @@ export const serverEnv = serverEnvValidation.parse({
 	DEV: dev,
 	CSRF_CHECK_ORIGIN: env.CSRF_CHECK_ORIGIN,
 	CONFIG_FILE: env.CONFIG_FILE,
-	DEBUG: env.DEBUG
+	DEBUG: env.DEBUG,
+	DEBUG_CLASSES: env.DEBUG_CLASSES
 });
 
-if (serverEnv.DEBUG) console.log('Server Env', serverEnv);
+type logClasses = 'ERROR' | 'WARN' | 'INFO' | 'DEBUG' | 'TRACE';
+type logParams = Parameters<typeof console.log>;
+export const debugFunction = (logClass: logClasses = 'INFO', ...params: logParams) => {
+	if (serverEnv.DEBUG) console.log(logClass, ...params);
+};
+
+export const debug = {
+	error: (...params: logParams) => {
+		if (serverEnv.DEBUG_CLASSES.includes('ERROR')) debugFunction('ERROR', ...params);
+	},
+	warn: (...params: logParams) => {
+		if (serverEnv.DEBUG_CLASSES.includes('WARN')) debugFunction('WARN', ...params);
+	},
+	info: (...params: logParams) => {
+		if (serverEnv.DEBUG_CLASSES.includes('INFO')) debugFunction('INFO', ...params);
+	},
+	debug: (...params: logParams) => {
+		if (serverEnv.DEBUG_CLASSES.includes('DEBUG')) debugFunction('DEBUG', ...params);
+	},
+	trace: (...params: logParams) => {
+		if (serverEnv.DEBUG_CLASSES.includes('TRACE')) debugFunction('TRACE', ...params);
+	}
+};
+
+debug.info('Server Environment:', serverEnv);
